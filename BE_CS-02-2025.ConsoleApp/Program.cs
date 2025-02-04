68e2e856710573b2dc107037d9dabc6c75fea572
TheAnhDev369 <theanhdev369@gmail.com>
2025-02-04 11:43:23 +0700

    first commit

---
+++ BE_CS-02-2025.ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE_CS_02_2025.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello");
+        }
+    }
+}
